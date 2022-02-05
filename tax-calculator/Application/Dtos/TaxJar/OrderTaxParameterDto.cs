0702ef38c247a45f44c3076c0c4f1964db9ca72b
Ronaldo Patterson <ronaldoapatterson@outlook.com>
2022-02-05 00:25:29 -0500

    Adds validations to input dto parameters

--- tax-calculator/Application/Dtos/TaxJar/OrderTaxParameterDto.cs
+++ tax-calculator/Application/Dtos/TaxJar/OrderTaxParameterDto.cs
@@ -1,14 +1,20 @@
-﻿namespace Application.Dtos.TaxJar
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.Dtos.TaxJar
 {
     public class OrderTaxParameterDto
     {
         public string FromCountry { get; set; }
         public string FromZipCode { get; set; }
         public string FromState { get; set; }
+
+        [Required]
         public string ToCountry { get; set; }
         public string ToZip { get; set; }
         public string ToState { get; set; }
         public double Amount { get; set; }
+
+        [Required]
         public double Shipping { get; set; }
         public IEnumerable<LineItemDto> LineItems { get; set; }
     }
